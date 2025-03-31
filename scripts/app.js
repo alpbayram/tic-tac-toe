5825e3d14439b271d89438ae1f83b29756c5ecd8
AlpBayram <57071283+alpbayram@users.noreply.github.com>
2025-03-31 21:15:12 +0300

    Methodların yerlerini değiştirdim ve dom methodları ekledim. Arayüz yaptım.

--- scripts/app.js
+++ scripts/app.js
@@ -22,23 +22,70 @@ const gameMethods = (function () {
 		const turn = round % 2 == 0 ? "X" : "O";
 		return `Şuanki olduğunuz round ${round} ve sıra ${turn} kişisinde.`;
 	};
+
 	const showBoard = function (gameBoard) {
 		return `${gameBoard[0][0]} | ${gameBoard[0][1]} | ${gameBoard[0][2]}\n${gameBoard[1][0]} | ${gameBoard[1][1]} | ${gameBoard[1][2]}\n${gameBoard[2][0]} | ${gameBoard[2][1]} | ${gameBoard[2][2]}`;
 	};
 
+
+	
+
 	const getPlayersNames = function (playerOneName, playerTwoName) {
 		return `Oyuncularımız ${playerOneName} ve ${playerTwoName}.`;
 	};
+
+	const checkWinning = function (mark, gameBoard) {
+		let checkRow = gameBoard.find(function (item, index, array) {
+			let xCountInRow = item.filter(function (item, index, array) {
+				if (item == mark) {
+					return true;
+				}
+			}).length;
+
+			if (xCountInRow == 3) {
+				return true;
+			}
+		});
+		let returnColumn = (function (marker) {
+			function column(element, index, array) {
+				let checkColumn = gameBoard.reduce(function (acc, item, index, array) {
+					if (item[element] == marker) {
+						acc.push(item[element]);
+						console.log(acc);
+						return acc;
+					} else {
+						return acc;
+					}
+				}, []).length;
+				if (checkColumn == 3) {
+					return true;
+				}
+			}
+			if ([0, 1, 2].some(column) == true) {
+
+				return true;
+			}
+		})(mark);
+
+		if (checkRow != undefined || returnColumn == true) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
 	return {
 		getRound,
 		showBoard,
 		getPlayersNames,
+		checkWinning,
 	};
 })();
 
+// Instance Factory Function'ım
 function playGame({ playerOneMarker, playerOneName, playerTwoMarker, playerTwoName }) {
 	let round = 0;
-
+	let isGameOver = false;
 	const gameBoard = [
 		[null, null, null],
 		[null, null, null],
@@ -46,92 +93,76 @@ function playGame({ playerOneMarker, playerOneName, playerTwoMarker, playerTwoNa
 	];
 
 	return function gameFlow() {
-		const getRound = gameMethods.getRound(round);
-		const showBoard = gameMethods.showBoard(gameBoard);
+		const getRound = () => gameMethods.getRound(round);
+		const showBoard = () => gameMethods.showBoard(gameBoard);
 		const getPlayersNames = gameMethods.getPlayersNames(playerOneName, playerTwoName);
+		const isCellEmpty = function(y,x){
+			return gameBoard[y][x]==null?true:false
+		}
+		const gameOver = () =>
+			isGameOver == true ? true:false;
+		const turn = () =>
+			round % 2 == 0 ? [playerOneMarker, playerOneName] : [playerTwoMarker, playerTwoName];
+		console.log(turn());
+		const playOneRound = function (y, x) {
+			if (isGameOver) {
+				console.log("oyun bitti lütfen yeni oyun başlatınız");
+				return showBoard();
+			}
 
-		const playOneRound = function () {
-			if (round % 2 == 0) {
-				const y = prompt("oynıcagınız yeri seçiniz");
-				const x = prompt("oynıcagınız yeri seçiniz");
-				if (playerOneMarker == "x" && gameBoard[y][x] == null) {
-					gameBoard[y][x] = "x";
-					console.log(checkWinning(playerOneMarker));
-					if (checkWinning(playerOneMarker) == true) {
-						console.log(
-							`${playerOneName} siz kazandınız. İşaretiniz: ${playerOneMarker}`
-						);
-					} else {
-						round++;
-					}
+			if (gameBoard[y][x] == null) {
+				gameBoard[y][x] = turn()[0];
+
+				if (gameMethods.checkWinning(turn()[0], gameBoard) == true) {
+					isGameOver = gameMethods.checkWinning(turn()[0], gameBoard);
+					console.log(`${turn()[1]} siz kazandınız. İşaretiniz: ${turn()[0]}`);
 				} else {
-					console.log("orayı seçemezsiniz zaten seçildi lütfen tekrar yer seçin");
+					
+					round++;
+				
 				}
 			} else {
-				const y = prompt("oynıcagınız yeri seçiniz");
-				const x = prompt("oynıcagınız yeri seçiniz");
-				if (playerTwoMarker == "o" && gameBoard[y][x] == null) {
-					gameBoard[y][x] = "o";
-					console.log(checkWinning(playerTwoMarker));
-					if (checkWinning(playerTwoMarker) == true) {
-						console.log(
-							`${playerTwoName} siz kazandınız. İşaretiniz: ${playerTwoMarker}`
-						);
-					} else {
-						round++;
-					}
-				} else {
-					console.log("orayı seçemezsiniz zaten seçildi lütfen tekrar yer seçin");
-				}
+				console.log("orayı seçemezsiniz zaten seçildi lütfen tekrar yer seçin");
 			}
+
 			return showBoard();
 		};
 
-		const checkWinning = function (mark) {
-			let checkRow = gameBoard.find(function (item, index, array) {
-				let xCountInRow = item.filter(function (item, index, array) {
-					if (item == mark) {
-						return true;
-					}
-				}).length;
-
-				if (xCountInRow == 3) {
-					return true;
-				}
-			});
-			let returnColumn = (function (marker) {
-				let checkColumn = 0;
-				let col = 0;
-				for (; checkColumn < 3; ) {
-					if (checkColumn === 3 || col == 2) {
-						break;
-					} else {
-						col++;
-					}
-					checkColumn = gameBoard.reduce(function (acc, item, index, array) {
-						if (item[col] == marker) {
-							acc.push(item[col]);
-							console.log(acc);
-							return acc;
-						} else {
-							return acc;
-						}
-					}, []).length;
-				}
-
-				if (checkColumn == 3) {
-					return true;
-				}
-			})(mark);
-
-			if (checkRow != undefined || returnColumn == true) {
-				return true;
-			} else {
-				return false;
-			}
-		};
-
-		return { playOneRound, showBoard, getPlayersNames, checkWinning, getRound };
+		return { playOneRound, showBoard, getPlayersNames, getRound, turn, isGameOver,isCellEmpty,gameOver };
 	};
 }
+
 const game1 = playGame(players);
+const gameFlowInstance = game1();
+const domMethods = (function () {
+	const container = document.querySelector(".container");
+
+	container.addEventListener("click", renderDom);
+
+	function renderDom(event) {
+		if (event.target.closest(".pushable")) {
+			event.preventDefault();
+			const imageSource = event.target.querySelector(".slot-img");
+			const coordinateY = event.target.closest(".pushable").dataset.y;
+			const coordinateX = event.target.closest(".pushable").dataset.x;
+			// console.log(coordinateY, coordinateX);
+			// console.log(event.target);
+			
+			if (gameFlowInstance.gameOver() == false && gameFlowInstance.isCellEmpty(coordinateY,coordinateX)==true) {
+				console.log(gameFlowInstance.gameOver())
+				console.log(imageSource)
+				if(gameFlowInstance.turn()[0]=="x"){
+					imageSource.src="../images/x.svg"
+				}else{
+					imageSource.src="../images/o.svg"
+				}
+				// event.target.textContent = gameFlowInstance.turn()[0];
+				gameFlowInstance.playOneRound(coordinateY, coordinateX);
+			} else {
+				console.log("bitti");
+			}
+		}
+	}
+
+	return {};
+})();
