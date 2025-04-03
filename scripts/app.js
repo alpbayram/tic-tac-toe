aa40b6f69851acba33ab4b92f742a9660ff0d975
AlpBayram <57071283+alpbayram@users.noreply.github.com>
2025-04-03 05:21:46 +0300

    Arayüz hatalarını giderdim. Dialog ekledim. Input validation ekledim ön yüz için.

--- scripts/app.js
+++ scripts/app.js
@@ -152,23 +152,52 @@ const domMethods = (function () {
 	const nameContainer = document.querySelector(".name-container");
 	const input1 = nameContainer.querySelector("#player-1-name");
 	const input2 = nameContainer.querySelector("#player-2-name");
+	const dialog = document.querySelector("dialog");
+	const dialogText = dialog.querySelector(".dialog-text");
+	const dialogButton = dialog.querySelector(".dialog-button");
+	const slotAll = document.querySelectorAll(".slot-img");
+	const markX = document.querySelector(".x-select");
+	const markO = document.querySelector(".o-select");
 	input1.value = "Player1";
 	input2.value = "Player2";
+
+	dialogButton.addEventListener("click", dialogButtonFunction);
+
+	function dialogButtonFunction(event) {
+		const getValue = markX.dataset.value == "1" ? "x" : "o";
+		const players = createPlayers({
+			playerOneName: input1.value,
+			playerOneMarker: getValue,
+			playerTwoName: input2.value,
+		});
+		gameFlowInstance = playGame(players);
+		slotAll.forEach((element) => {
+			element.src = "";
+		});
+		dialog.close();
+	}
+	nameContainer.addEventListener("input", updateValue);
+	function updateValue(event) {
+		if (event.target.matches("#player-1-name")) {
+			event.target.value = event.target.value.trim();
+		} else if (event.target.matches("#player-2-name")) {
+			event.target.value = event.target.value.trim();
+		}
+	}
+
 	createGameContainer.addEventListener("click", createDom);
 	container.addEventListener("click", renderDom);
 
 	function createDom(event) {
-		const regex = new RegExp("^[A-Za-zşŞıİçÇöÖüÜĞğ\d]{3,12}$")
-		const markX = document.querySelector(".x-select");
-		const markO = document.querySelector(".o-select");
+		const regex = /^[A-Za-zşŞıİçÇöÖüÜĞğ\d]{3,12}$/;
 
 		const player1Stat = container.querySelector(".player-1-stat");
 		const player2Stat = container.querySelector(".player-2-stat");
 		if (event.target.closest(".mark-select")) {
 			const markContainer = event.target.closest(".mark-select");
 			const relative = markContainer.querySelector(".relative-switch");
-			console.log(input1.value);
-			if (event.target.closest(".x-select")) {
+
+			if (event.target.matches(".x-select") || event.target.matches(".mark-X")) {
 				markX.dataset.value = "1";
 				markO.dataset.value = "0";
 				relative.style.transform = "translate(0%, 0%)";
@@ -192,15 +221,14 @@ const domMethods = (function () {
 				player2Stat.textContent = `${gameFlowInstance.getPlayersNames[3]} (${gameFlowInstance.getPlayersNames[2]}) `;
 				mark.textContent = `Turn: ${gameFlowInstance.turn()[0]}`;
 				console.log(getValue);
-			}
-			else{
-				console.log("burada")
+			} else {
+				input2.reportValidity();
+				input1.reportValidity();
 			}
 		}
 	}
 
 	function renderDom(event) {
-		const slotAll = document.querySelectorAll(".slot-img");
 		if (event.target.closest(".restart")) {
 			console.log("girdi");
 			createGameContainer.style.display = "flex";
@@ -212,8 +240,9 @@ const domMethods = (function () {
 			});
 		} else if (event.target.closest(".pushable")) {
 			event.preventDefault();
-			mark.textContent = `Turn: ${gameFlowInstance.turn()[0]}`;
+
 			const imageSource = event.target.querySelector(".slot-img");
+			const winnerSource = document.querySelector(".winner-img");
 			const coordinateY = event.target.closest(".pushable").dataset.y;
 			const coordinateX = event.target.closest(".pushable").dataset.x;
 			// console.log(coordinateY, coordinateX);
@@ -237,9 +266,16 @@ const domMethods = (function () {
 
 				// event.target.textContent = gameFlowInstance.turn()[0];
 				gameFlowInstance.playOneRound(coordinateY, coordinateX);
+				console.log(gameFlowInstance.gameOver() == true);
 				if (gameFlowInstance.gameOver() == true) {
 					mark.style.fontSize = "1.1rem";
-
+					dialog.showModal();
+					console.log(gameFlowInstance.getWinner()[1] + "bu");
+					winnerSource.src =
+						gameFlowInstance.getWinner()[1] == "x" ? "images/x.svg" : "images/o.svg";
+					dialogText.style.color =
+						gameFlowInstance.getWinner()[1] == "x" ? "#31c4be" : "#f2b237";
+					dialogText.textContent = `${gameFlowInstance.getWinner()[0]} TAKES THE ROUND`;
 					mark.textContent = `Winner: ${gameFlowInstance.getWinner()[0]}`;
 				} else {
 					mark.textContent = `Turn: ${gameFlowInstance.turn()[0]}`;
